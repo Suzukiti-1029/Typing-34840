7f11485b3c18458843fb0ae05b6a7a353cb1bd98
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-13 17:40:56 +0900

    ビューの単語がランダムになるように設定

--- app/controllers/typings_controller.rb
+++ app/controllers/typings_controller.rb
@@ -1,4 +1,7 @@
 class TypingsController < ApplicationController
   def index
+    number = rand(1..100)
+    @english_word = English.find(number).name
+    @japanese_word = Japanese.find(number).name
   end
 end
