986d82d713b545f60f1e561a5357a9732ab60d60
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-13 18:04:25 +0900

    newアクションの設定

--- app/controllers/typings_controller.rb
+++ app/controllers/typings_controller.rb
@@ -2,6 +2,13 @@ class TypingsController < ApplicationController
   def index
     number = rand(1..100)
     @english_word = English.find(number).name
-    @japanese_word = Japanese.find(number).name
+    @japanese_word = Japanese.find(@umber).name
+  end
+
+  def new
+    @number = rand(1..100)
+    @english_word = English.find(@number).name
+    @japanese_word = Japanese.find(@number).name
+    render json:{ english_word: @english_word, japanese_word: @japanese_word }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'typings#index'
+  resources :typings, only: [:new]
 end
