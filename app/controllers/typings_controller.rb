9dd6bbbe7fa4a7bcb656fe39e95f9ae66199e6a3
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-14 12:38:54 +0900

    問題のindexを表示

--- app/controllers/typings_controller.rb
+++ app/controllers/typings_controller.rb
@@ -1,14 +1,18 @@
 class TypingsController < ApplicationController
   def index
-    number = rand(1..100)
-    @english_word = English.find(number).name
-    @japanese_word = Japanese.find(number).name
+    @number = rand(1..100)
+    @english_word = English.find(@number).name
+    @japanese_word = Japanese.find(@number).name
   end
 
   def new
     number = rand(1..100)
     english_word = English.find(number).name
     japanese_word = Japanese.find(number).name
-    render json:{ english_word: english_word, japanese_word: japanese_word }
+    render json:{
+      number: number,
+      english_word: english_word,
+      japanese_word: japanese_word
+    }
   end
 end
--- app/javascript/typing.js
+++ app/javascript/typing.js
@@ -1,7 +1,8 @@
 function key_down (e) {
   e.preventDefault();
-  const inputWord = document.getElementById('input_word');
   const questionEnglishWord = document.getElementById('question_english_word');
+  const inputWord = document.getElementById('input_word');
+
   if (e.code == 'Backspace' || e.code == 'Delete') {
     inputWord.innerHTML = inputWord.innerHTML.slice(0, -1)
   }
@@ -18,15 +19,19 @@ function key_down (e) {
 function set_text () {
   const inputWord = document.getElementById('input_word');
   inputWord.innerHTML = ''
+
   const XHR = new XMLHttpRequest();
   XHR.open("GET", "/typings/new", true);
   XHR.responseType = "json";
   XHR.send();
   XHR.onload = () => {
+    const questionNumber = document.getElementById('question_number');
     const questionEnglishWord = document.getElementById('question_english_word');
     const questionJapaneseWord = document.getElementById('question_japanese_word');
+    const newNumber = XHR.response.number;
     const newEnglishWord = XHR.response.english_word;
     const newJapaneseWord = XHR.response.japanese_word;
+    questionNumber.innerHTML = newNumber;
     questionEnglishWord.innerHTML = newEnglishWord;
     questionJapaneseWord.innerHTML = newJapaneseWord;
   }
