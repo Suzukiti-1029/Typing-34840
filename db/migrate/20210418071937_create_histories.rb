9ffdc355ec2b638a56cf3b907e32110072ce7dc9
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-04-19 20:29:26 +0900

    historiesテーブルの作成

--- db/migrate/20210418071937_create_histories.rb
+++ db/migrate/20210418071937_create_histories.rb
@@ -2,6 +2,7 @@ class CreateHistories < ActiveRecord::Migration[6.0]
   def change
     create_table :histories do |t|
       t.integer :score, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
